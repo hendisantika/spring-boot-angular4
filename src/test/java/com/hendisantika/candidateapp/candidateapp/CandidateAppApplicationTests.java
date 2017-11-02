d8fb43933f71fcec6da7e1d5a7572a277d85b56e
Hendi Santika <hendisantika@gmail.com>
2017-11-03 05:27:34 +0700

    #1 - Initial Commit - candidate-app

---
+++ src/test/java/com/hendisantika/candidateapp/candidateapp/CandidateAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.candidateapp.candidateapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CandidateAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
