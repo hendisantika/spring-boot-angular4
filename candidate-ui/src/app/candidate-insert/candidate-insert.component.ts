bf2dfbf8cfaf7a2590f00cf46285a70a16dc4f4a
Hendi Santika <hendisantika@gmail.com>
2017-11-06 22:39:14 +0700

    #21 - Modified Candidate Component Insert TS

--- candidate-ui/src/app/candidate-insert/candidate-insert.component.ts
+++ candidate-ui/src/app/candidate-insert/candidate-insert.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Candidate} from "../../interfaces/candidate";
 import {CandidateService} from "../../services/candidate.service";
+import {Router} from "@angular/router";
+// import {LoaderService} from "../../services/loader.service";
 
 @Component({
   selector: 'app-candidate-insert',
@@ -11,9 +13,35 @@ export class CandidateInsertComponent implements OnInit {
 
   candidate: Candidate = new Candidate();
 
-  constructor(private candidateService : CandidateService) { }
+  constructor(private candidateService: CandidateService) { }
 
   ngOnInit() {
+  }
+
+  onProcessSaved(){
+    this.candidate.photo = 'test';
+    this.candidateService.save(this.candidate).subscribe(output => {
+      console.log(output);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  handleFileSelect(evt) {
+    var files: evt.target.files;
+    var file: files[0];
+
+    if (files && file) {
+      var reader = new FileReader();
+      reader.onload = this.handleReaderLoaded.bind(this);
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  handleReaderLoaded(readerEvt) {
+    var binaryString = readerEvt.target.result;
+    this.candidate.photo = btoa(binaryString);
+    console.log(this.candidate.photo);
   }
 
 }
