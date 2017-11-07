1889f44d7a27dc59c54a3567e5b903329fe99646
Hendi Santika <hendisantika@gmail.com>
2017-11-08 05:40:07 +0700

    #23 - Add Candidate List html

--- candidate-ui/src/app/app.module.ts
+++ candidate-ui/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CandidateInsertComponent } from './candidate-insert/candidate-insert.component';
 import {CandidateService} from "../services/candidate.service";
+import { CandidateListComponent } from './candidate-list/candidate-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CandidateInsertComponent
+    CandidateInsertComponent,
+    CandidateListComponent
   ],
   imports: [
     BrowserModule,
